90bd34c3033e7c32131f7faeb5caac13c7b63b93
player_legend <example.com>
2025-10-28 22:16:31 -0500

    Added a filesystem test for images

---
+++ src/engine/image/test/image.load.filesystem/main.cpp
@@ -0,0 +1,22 @@
+#include <engine/image.hpp>
+#include <assert.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    assert(argc == 2);
+
+    std::string image_path = argv[1];
+
+    std::string image_dir = std::filesystem::path(image_path).parent_path().string();
+
+    filesystem::whitelist whitelist(image_dir);
+
+    image::rgba32_cache cache(whitelist);
+
+    const image::rgba32 & image(*cache[image_path]);
+
+    std::cout << "Image dimensions: " << image.width << "x" << image.height << "\n";
+
+    std::cout << "Success for \n" << argv[0] << "\n";
+}
