26c9a7de8ab87c0a8c7f6579bea669d24fcfda90
player_legend <example.com>
2025-11-01 00:34:58 -0500

    Added a gpu component with some vertex attribute types

---
+++ src/engine/gpu/include/engine/gpu/types.hpp
@@ -0,0 +1,14 @@
+#include <engine/vec.hpp>
+#include <stdint.h>
+
+namespace engine::gpu::arrays
+{
+using position = vec::fvec3;
+using normal = vec::vec3<int16_t>;
+using tangent = vec::vec4<int16_t>;
+using texcoord = vec::vec2<uint16_t>;
+using color = vec::vec4<uint8_t>;
+using joints = vec::vec4<uint8_t>;
+using weights = vec::vec4<uint8_t>;
+using index = uint32_t;
+} // namespace engine::gpu::arrays
