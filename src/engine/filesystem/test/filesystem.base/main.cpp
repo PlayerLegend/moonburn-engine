87d7b1e737a4d52cbc5e91bb2082848ab9ea7f58
player_legend <example.com>
2025-11-01 00:33:46 -0500

    Filled in the filesystem.base test

--- src/engine/filesystem/test/filesystem.base/main.cpp
+++ src/engine/filesystem/test/filesystem.base/main.cpp
@@ -1,4 +1,38 @@
-int main()
+#include <assert.h>
+#include <engine/filesystem.hpp>
+#include <filesystem>
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
-    return 1; // todo
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <path-to-file>\n";
+        return 1;
+    }
+    std::filesystem::path file_path = argv[1];
+
+    filesystem::whitelist wl(file_path.parent_path().string());
+    filesystem::cache_binary cache(wl);
+    filesystem::cache_binary::reference ref = cache[file_path.string()];
+    if (!ref)
+    {
+        std::cerr << "Failed to load file: " << file_path.string() << "\n";
+        return 2;
+    }
+    const filesystem::allocation &alloc = *ref;
+    std::cout << "Loaded file: " << file_path.string() << " (" << alloc.size()
+              << " bytes)\n";
+
+    std::string file_str(alloc.begin(), alloc.end());
+
+    if (file_str != "Test file contents")
+    {
+        std::cerr << "File contents unexpected: " << file_str << "\n";
+        return 3;
+    }
+
+    std::cout << "Success for \n" << argv[0] << "\n";
+
+    return 0;
 }
