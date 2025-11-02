b8707864068fa9fa41d0f5ac6966057be277a9c4
player_legend <example.com>
2025-11-01 20:25:03 -0500

    Added gpu mesh primitive

--- src/engine/gpu/include/engine/gpu.hpp
+++ src/engine/gpu/include/engine/gpu.hpp
@@ -1,5 +1,11 @@
+#include <engine/exception.hpp>
 #include <engine/vec.hpp>
 #include <stdint.h>
+
+namespace gltf
+{
+class mesh_primitive;
+}
 
 namespace engine::gpu::attributes
 {
@@ -11,11 +17,32 @@ using color = vec::vec4<uint8_t>;
 using joints = vec::vec4<uint8_t>;
 using weights = vec::vec4<uint8_t>;
 using index = uint32_t;
-} // namespace engine::gpu::arrays
+} // namespace engine::gpu::attributes
 
 namespace engine::gpu
 {
-    class primitive {
-        uint32_t 
-    };
-}
+class primitive
+{
+    uint32_t vao = 0;
+    uint32_t vbo = 0;
+    uint32_t ibo = 0;
+    uint32_t count = 0;
+    bool short_indices = false;
+
+  public:
+    primitive(const class gltf::mesh_primitive &primitive);
+    ~primitive();
+
+    void draw();
+    void bind();
+};
+} // namespace engine::gpu
+
+namespace engine::gpu::exception
+{
+class base : public engine::exception
+{
+  public:
+    base(const std::string &message) : engine::exception(message) {}
+};
+} // namespace engine::gpu::exception
---
+++ src/engine/gpu/src/gpu.cpp
@@ -0,0 +1,197 @@
+#include <array>
+#include <engine/gltf.hpp>
+#include <engine/gpu.hpp>
+#include <glad/glad.h>
+
+#define gl_check_error()                                                       \
+    {                                                                          \
+        GLuint error;                                                          \
+        if (GL_NO_ERROR != (error = glGetError()))                             \
+            throw engine::gpu::exception::base("GL error " +                   \
+                                               std::to_string(error));         \
+    }
+
+#define gl_call(name, ...)                                                     \
+    {                                                                          \
+        GLuint error;                                                          \
+        name(__VA_ARGS__);                                                     \
+                                                                               \
+        if (GL_NO_ERROR != (error = glGetError()))                             \
+            throw engine::gpu::exception::base(                                \
+                "GL error " + std::to_string(error) +                          \
+                " after calling function " #name);                             \
+    }
+
+engine::gpu::primitive::primitive(const class gltf::mesh_primitive &input)
+{
+    gl_check_error();
+
+    struct vertex_attribute
+    {
+        const gltf::accessor *accessor;
+        uint32_t index;
+        bool normalized;
+        gltf::component_type component_type;
+        gltf::attribute_type attribute_type;
+    };
+
+    std::array<vertex_attribute, 6> attributes = {
+        {{.accessor = input.attributes.position,
+          .index = 0,
+          .normalized = false,
+          .component_type = gltf::component_type::FLOAT,
+          .attribute_type = gltf::attribute_type::VEC3},
+         {.accessor = input.attributes.normal,
+          .index = 1,
+          .normalized = true,
+          .component_type = gltf::component_type::SHORT,
+          .attribute_type = gltf::attribute_type::VEC3},
+         {.accessor = input.attributes.tangent,
+          .index = 2,
+          .normalized = true,
+          .component_type = gltf::component_type::SHORT,
+          .attribute_type = gltf::attribute_type::VEC4},
+         {.accessor = input.attributes.texcoord_0,
+          .index = 3,
+          .normalized = true,
+          .component_type = gltf::component_type::USHORT,
+          .attribute_type = gltf::attribute_type::VEC2},
+         {.accessor = input.attributes.joints,
+          .index = 4,
+          .normalized = false,
+          .component_type = gltf::component_type::UBYTE,
+          .attribute_type = gltf::attribute_type::VEC4},
+         {.accessor = input.attributes.weights,
+          .index = 5,
+          .normalized = true,
+          .component_type = gltf::component_type::UBYTE,
+          .attribute_type = gltf::attribute_type::VEC4}}};
+
+    gl_call(glGenBuffers, 1, &vbo);
+    gl_call(glGenVertexArrays, 1, &vao);
+    gl_call(glBindVertexArray, vao);
+
+    if (input.indices)
+    {
+        gl_call(glGenBuffers, 1, &ibo);
+        gl_call(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+        if (input.indices->count < std::numeric_limits<uint16_t>::max())
+        {
+            std::vector<uint16_t> indices = *input.indices;
+            gl_call(glBufferData,
+                    GL_ELEMENT_ARRAY_BUFFER,
+                    indices.size() * sizeof(uint16_t),
+                    indices.data(),
+                    GL_STATIC_DRAW);
+            short_indices = true;
+        }
+        else
+        {
+            std::vector<uint32_t> indices = *input.indices;
+            gl_call(glBufferData,
+                    GL_ELEMENT_ARRAY_BUFFER,
+                    indices.size() * sizeof(uint32_t),
+                    indices.data(),
+                    GL_STATIC_DRAW);
+            short_indices = false;
+        }
+        count = input.indices->count;
+    }
+    else
+    {
+        if (!input.attributes.position)
+            throw engine::gpu::exception::base(
+                "Cannot create GPU primitive without position accessor or "
+                "indices");
+        count = input.attributes.position->count;
+    }
+
+    gl_call(glBindBuffer, GL_ARRAY_BUFFER, vbo);
+
+    std::vector<uint8_t> vertex_data;
+
+    for (const vertex_attribute &attribute : attributes)
+    {
+        if (!attribute.accessor)
+            continue;
+
+        if (attribute.accessor->component_type == gltf::component_type::FLOAT)
+        {
+            gl_call(glVertexAttribPointer,
+                    attribute.index,
+                    (GLint)attribute.attribute_type,
+                    (GLenum)attribute.component_type,
+                    attribute.normalized,
+                    0,
+                    (void *)vertex_data.size());
+        }
+        else
+        {
+            gl_call(glVertexAttribIPointer,
+                    attribute.index,
+                    (GLint)attribute.attribute_type,
+                    (GLenum)attribute.component_type,
+                    0,
+                    (void *)vertex_data.size());
+        }
+        attribute.accessor->dump(vertex_data,
+                                 attribute.component_type,
+                                 attribute.attribute_type);
+    }
+
+    gl_call(glBindBuffer, GL_ARRAY_BUFFER, vbo);
+    gl_call(glBufferData,
+            GL_ARRAY_BUFFER,
+            vertex_data.size(),
+            vertex_data.data(),
+            GL_STATIC_DRAW);
+
+    gl_call(glBindVertexArray, 0);
+    gl_call(glBindBuffer, GL_ARRAY_BUFFER, 0);
+}
+
+engine::gpu::primitive::~primitive()
+{
+    if (ibo)
+        glDeleteBuffers(1, &ibo);
+    if (vbo)
+        glDeleteBuffers(1, &vbo);
+    if (vao)
+        glDeleteVertexArrays(1, &vao);
+}
+
+void engine::gpu::primitive::bind()
+{
+    gl_call(glBindVertexArray, vao);
+    gl_call(glBindBuffer, GL_ARRAY_BUFFER, vbo);
+    if (ibo)
+        gl_call(glBindBuffer, GL_ELEMENT_ARRAY_BUFFER, ibo);
+}
+
+void engine::gpu::primitive::draw()
+{
+    if (ibo)
+    {
+        if (short_indices)
+        {
+            gl_call(glDrawElements,
+                    GL_TRIANGLES,
+                    count,
+                    GL_UNSIGNED_SHORT,
+                    nullptr);
+        }
+        else
+        {
+            gl_call(glDrawElements,
+                    GL_TRIANGLES,
+                    count,
+                    GL_UNSIGNED_INT,
+                    nullptr);
+        }
+    }
+    else
+    {
+        gl_call(glDrawArrays, GL_TRIANGLES, 0, count);
+    }
+}
