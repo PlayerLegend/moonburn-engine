5fb5a2d5f42a0468e2461c8966575bc5fa5c9c89
player_legend <example.com>
2025-11-03 16:07:48 -0600

    Expanded the platform.base test to print button presses/releases, removed it from the auto testing list because it's necessarily interactive now

--- src/engine/platform/test/platform.base/main.cpp
+++ src/engine/platform/test/platform.base/main.cpp
@@ -1,10 +1,33 @@
 #include <engine/platform.hpp>
+#include <iostream>
 
 int main()
 {
     platform::window window("Platform Base Test");
 
-    platform::frame::state frame = window.get_frame();
+    while (true)
+    {
+        platform::frame::state frame = window.get_frame();
+        if (frame.should_close)
+            break;
+
+        for (auto event : frame.events)
+        {
+            if (!std::holds_alternative<platform::event::button>(event))
+                continue;
+
+            platform::event::button &button_event =
+                std::get<platform::event::button>(event);
+
+            std::cout << "Button "
+                      << platform::button::id_from_string(button_event.id)
+                      << " is "
+                      << (button_event.state & platform::button::PRESSED
+                              ? "PRESSED"
+                              : "RELEASED")
+                      << std::endl;
+        }
+    }
 
     return 0;
 }
